841205b2e4fcad00fb0b98eb164b767dc535b8cf
loureiroclubecerto <ti2@clubecerto.com.br>
2022-02-21 17:14:31 -0300

    adds icon to page title

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
+import CableIcon from '@mui/icons-material/Cable';
 import Container from '@mui/material/Container';
+import SvgIcon from '@mui/material/SvgIcon';
 import Typography from '@mui/material/Typography';
 
 const PageTitle = () => {
   return (
     <Container
       sx={{
+        alignItems: "center",
         backgroundColor: "#00964f",
         borderRadius: "10px",
         color: "white",
+        display: "flex",
         my: 3,
         py: 2,
       }}
     >
+      <SvgIcon fontSize="large" sx={{ mr: 1 }}>
+        <CableIcon />
+      </SvgIcon>
       <Typography
         component="h1"
         fontWeight="600"
